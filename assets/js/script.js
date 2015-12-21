a0491751249f12844a77839b67d9c023e34359db
allen-blue <polandeme@gmail.com>
2015-12-21 19:53:04 +0800

    fix title

--- assets/js/script.js
+++ assets/js/script.js
@@ -118,21 +118,21 @@ function afterPjax() {
 
   // Lazy Loading Disqus
   // http://jsfiddle.net/dragoncrew/SHGwe/1/
-  var ds_loaded = false,
-      top = $('#disqus_thread').offset().top;
-  window.disqus_shortname = $('#disqus_thread').attr('name');
+//   var ds_loaded = false,
+//       top = $('#disqus_thread').offset().top;
+//   window.disqus_shortname = $('#disqus_thread').attr('name');
 
-  function check() {
-    if ( !ds_loaded && container.scrollTop() + container.height() > top ) {
-      $.ajax({
-        type: 'GET',
-        url: 'http://' + disqus_shortname + '.disqus.com/embed.js',
-        dataType: 'script',
-        cache: true
-      });
-      ds_loaded = true;
-    }
-  }check();
-  container.scroll(check);
-}afterPjax();
+//   function check() {
+//     if ( !ds_loaded && container.scrollTop() + container.height() > top ) {
+//       $.ajax({
+//         type: 'GET',
+//         url: 'http://' + disqus_shortname + '.disqus.com/embed.js',
+//         dataType: 'script',
+//         cache: true
+//       });
+//       ds_loaded = true;
+//     }
+//   }check();
+//   container.scroll(check);
+// }afterPjax();
 
